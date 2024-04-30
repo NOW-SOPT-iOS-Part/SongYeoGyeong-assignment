c34bb09e77f89f5d75fa6065e09b4453be7170eb
0gonge <Challengef0802@gmail.com>
2024-04-30 16:02:16 +0900

    [FEAT] Implement Under TabBar

---
+++ Assignment-clonecoding/Assignment-clonecoding/3rd/HomeController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeController.swift
+//  Assignment-clonecoding
+//
+//  Created by 송여경 on 4/30/24.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .black
+    }
+
+}
